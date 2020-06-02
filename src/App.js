920e6cc8f97da244b58ad257be9a9ae885d49f41
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-06-02 23:05:04 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scorer: '',
+      source: '',
+      game:'',
+      date: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://aqueous-bastion-92424.herokuapp.com/')
+      .then(response => {
+        return response.json();
+      })
+      .then(goal => {
+        this.setState(
+          {
+            scorer: goal["SCORER"],
+            source: goal["LINK"],
+            game: goal["GAME"],
+            date: goal["DATE"]
+          }
+        )
+      });
+  }
+
+  render() {
+    console.log(this.state);
+    const {game, source, scorer, date} = this.state;
+    return (
+      <div className="App">
+        <h1>{scorer}</h1>
+        <img src={source} alt="Gol"/>
+        <h2>{game}</h2>
+        <h3>{date}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
